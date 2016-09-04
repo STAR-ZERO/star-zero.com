10a7f85a1148b63cc3faca882740ad21bce3edde
STAR_ZERO <kenji.01.star@gmail.com>
2016-09-04 21:58:25 +0900

    Leave animation for loading

--- js/components/Loading.js
+++ js/components/Loading.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 export default class Loading extends React.Component {
   render() {
+    var loading = '';
+    if (!this.props.completedLoading) {
+      loading = (
+        <div id="circleG" className={this.props.completedLoading ? 'hidden' : ''}>
+          <div id="circleG_1" className="circleG"></div>
+          <div id="circleG_2" className="circleG"></div>
+          <div id="circleG_3" className="circleG"></div>
+        </div>
+      );
+    }
+
     return (
-      <div id="circleG" className={this.props.completedLoading ? 'hidden' : ''}>
-        <div id="circleG_1" className="circleG"></div>
-        <div id="circleG_2" className="circleG"></div>
-        <div id="circleG_3" className="circleG"></div>
-      </div>
+      <ReactCSSTransitionGroup transitionName="loading" transitionAppear={false} transitionEnter={false} transitionLeave={true} transitionLeaveTimeout={2000}>
+        {loading}
+      </ReactCSSTransitionGroup>
     )
   }
 }
