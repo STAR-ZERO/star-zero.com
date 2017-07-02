2a5f49a485698e6088e193e61725f1185ebb6db1
Kenji Abe <kenji.01.star@gmail.com>
2017-07-02 11:39:07 +0900

    Add link Medium, remove link Qiita

--- js/components/Link.js
+++ js/components/Link.js
@@ -10,10 +10,10 @@ export default class Link extends React.Component {
           <div className="link">
             <a href="https://twitter.com/STAR_ZERO" target="_blank"><img src="img/twitter.png" width="60" height="60" /></a>
             <a href="https://github.com/STAR-ZERO" target="_blank"><img src="img/github.png" width="60" height="60" /></a>
+            <a href="https://medium.com/@star_zero" target="_blank"><img src="img/medium.png" width="60" height="60" /></a>
             <a href="http://starzero.hatenablog.com/" target="_blank"><img src="img/hatenablog.png" width="60" height="60" /></a>
             <a href="http://www.amazon.co.jp/registry/wishlist/2PFG3VL74AWWJ" target="_blank"><img src="img/amazon.png" width="60" height="60" /></a>
             <a href="http://tumblr.star-zero.com/" target="_blank"><img src="img/tumblr.png" width="60" height="60" /></a>
-            <a href="http://qiita.com/STAR_ZERO" target="_blank"><img src="img/qiita.png" width="60" height="60" /></a>
           </div>
         </div>
       </ReactCSSTransitionGroup>
